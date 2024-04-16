643cffe0e76ad597c0c2a0aa6c3e141b123c35e6
Shivam Kumar <107680105+ShivamKumar-0512@users.noreply.github.com>
2024-04-17 00:07:34 +0530

    Delete YtServer directory

--- YtServer/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import URLField, StringField, SubmitField
-from wtforms.validators import DataRequired, Optional
-
-
-class PredictForm(FlaskForm):
-    url = URLField('Youtube URL', validators=[DataRequired()])
-    submit = SubmitField('Predict')
